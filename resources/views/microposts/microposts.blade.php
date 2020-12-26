205049fad6a1115d007aa835900513c9a8f9e287
naoto.isoda <naoto.isoda@offisis.co.jp>
2020-12-26 10:41:49 +0000

    btn modify

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -11,21 +11,25 @@
                     <div>
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    <div class="col-sm-2">
-                            @if(!\Auth::user()->is_favorited($micropost->id))
-                                {!! Form::open(['route'=>['micropost.favorite',$micropost->id],'method' => 'post']) !!}
-                                    {!! Form::submit('Favorite',['class' => 'btn btn-success btn-sm btn-block']) !!}
-                                {!! Form::close() !!}
-                            @else
-                                {!! Form::open(['route'=>['micropost.unfavorite',$micropost->id],'method' => 'delete']) !!}
-                                    {!! Form::submit('Unfavorite',['class' => 'btn btn-warning btn-sm btn-block']) !!}
-                                {!! Form::close() !!}
-                            @endif
-                            @if(Auth::id()==$micropost->user_id)
-                                {!! Form::open(['route'=>['microposts.destroy',$micropost->id],'method'=>'delete']) !!}
-                                    {!! Form::submit('Delete',['class'=>'btn btn-danger btn-sm btn-block']) !!}
-                                {!! Form::close() !!}
-                            @endif
+                    <div class="row">
+                        <div class="col-3">
+                                @if(!\Auth::user()->is_favorited($micropost->id))
+                                    {!! Form::open(['route'=>['micropost.favorite',$micropost->id],'method' => 'post']) !!}
+                                        {!! Form::submit('Favorite',['class' => 'btn btn-success btn-sm btn-block']) !!}
+                                    {!! Form::close() !!}
+                                @else
+                                    {!! Form::open(['route'=>['micropost.unfavorite',$micropost->id],'method' => 'delete']) !!}
+                                        {!! Form::submit('Unfavorite',['class' => 'btn btn-warning btn-sm btn-block']) !!}
+                                    {!! Form::close() !!}
+                                @endif
+                        </div>
+                        <div class="col-3">
+                                @if(Auth::id()==$micropost->user_id)
+                                    {!! Form::open(['route'=>['microposts.destroy',$micropost->id],'method'=>'delete']) !!}
+                                        {!! Form::submit('Delete',['class'=>'btn btn-danger btn-sm btn-block']) !!}
+                                    {!! Form::close() !!}
+                                @endif
+                        </div>
                     </div>
                 </div>
             </li>
